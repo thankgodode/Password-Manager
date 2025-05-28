51aaaf221210f243797b316bd3455075f8298af2
Thankgod Ode <thankgodode535@gmail.com>
2025-05-28 09:32:32 +0100

    kdls

--- client/src/components/Setting.js
+++ client/src/components/Setting.js
@@ -1,3 +1,5 @@
+/*global chrome*/
+
 import back_icon from "../img/arrow.svg";
 import { Link, useNavigate } from "react-router-dom";
 import {useState} from "react"
@@ -17,6 +19,16 @@ export default function Setting(props) {
       setIsLoading(true)
       const auth = await API.get("/logout",{withCredentials:true});
       localStorage.removeItem("token")
+
+      chrome.runtime.sendMessage(
+        "ifhimppppnnffofkmagbggildngckaol",
+        {
+          type:"LOGOUT_SUCCESS"
+        }, 
+        (response) => {
+          console.log("Response ", response)
+        }
+      )
 
       setMsg("User successfully logged out!")
       setIsLoading(false)
